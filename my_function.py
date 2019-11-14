5df1bfb152fe9051e2f084b0b7910bb2131aeb6d
jioongyy <36102827+jioongyy@users.noreply.github.com>
2019-11-14 16:28:31 +0900

    version 1.1     修改了从视频中获取数据集的方法
    slice_video: 
    （1）能从视频中获得更大的人脸区域，而不是原来的直接用opencv框定的一小块区域。避免因为只取到人脸而使得数据集过于简单
    （2）视频中如果出现多个人脸则取其中最大的人脸作为数据集
    （3）小规模地重写，部分功能单独写为函数放在my_function中
    my_function:增加了两个函数
    （1）primary_facial_area 获取主要人脸
    （2）bigger_area 获取更大人脸区域

--- my_function.py
+++ my_function.py
@@ -16,13 +16,24 @@ def random_choose(all, choose_count):
         for i in range(all):
             if i not in chosen_set:
                 unchosen_list.append(i)
-    return list(chosen_set),unchosen_list
+    return list(chosen_set), unchosen_list
 
 
-# print(np.zeros((3,3,3)))
-# array2 = []
-# for i in range(103):
-#     array1 = np.zeros((100,100))
-#     array2.append(array1)
-# print(array2)
-# print(np.array(array2))
+# 在rects数组中找到最大的一块脸部区域，并作为数组返回
+def primary_facial_area(rects):
+    biggest_area_value = 0
+    for x, y, w, h in rects:
+        area_now = w * h
+        if area_now > biggest_area_value:
+            biggest_area_value = area_now
+            biggest_area = [x, y, w, h]
+    return biggest_area
+
+
+# 输入一个opencv识别出来的区域的x,y,w,h，返回相比原来正方形各边变长，中心不变的更大的正方形的x,y,w,h
+def bigger_area(x, y, w, h, longer):
+    center = (x + w / 2, y + h / 2)
+    side = max(w, h) + longer
+    x = center[0] - side / 2
+    y = center[1] - side / 2
+    return x, y, side
--- slice_video.py
+++ slice_video.py
@@ -1,14 +1,16 @@
-from imutils.video import VideoStream
 import argparse
 import imutils
 import cv2
 import os
 import numpy as np
+import my_function
 
+# argument settings
 rotate_angle = 90
 goal_size = 100
 facial_recognize_side = 120
 sample_frequency = 15
+# ******************
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-c", "--cascade", required=True,
@@ -22,46 +24,36 @@ args = vars(ap.parse_args())
 detector = cv2.CascadeClassifier(args["cascade"])
 vs = cv2.VideoCapture(args['input'])
 
+
 total_frame = 0
 count = 0
-
 while (vs.isOpened()):
     ret, frame = vs.read()
+    # some videos direction is not correct cause opencv cannot recognize the face
+    # so need to rotate the video
     frame = imutils.rotate(frame, angle=rotate_angle)
-    orig = frame.copy()
     frame = imutils.resize(frame, width=400)
+    # opencv detect the face in the frame
     rects = detector.detectMultiScale(
         cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), scaleFactor=1.1,
-        minNeighbors=5, minSize=(30, 30)
+        minNeighbors=5, minSize=(facial_recognize_side, facial_recognize_side)
     )
-    # draw the rectangle in photo
-    # for (x, y, w, h) in rects:
-    #     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
 
-    # crop the photo to the recognize area
+    # crop the photo which including the recognize area
     if (len(rects) != 0):
-        side = max(rects[0][2], rects[0][3])
-        frame = frame[rects[0][1]:rects[0][1] + side, rects[0][0]:rects[0][0] + side]
+        # opencv will detect all face appeare in the frame,and every face will store as a element of array
+        # we need to find the biggest face area as the dataset from this array "rects"
+        facial_area = my_function.primary_facial_area(rects)
+        x, y, side = my_function.bigger_area(facial_area[0],facial_area[1],facial_area[2],facial_area[3])
+        frame = frame[y:y + side, x:x + side]
+        # convert the frame to grayscale
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         count += 1
-        if (side > facial_recognize_side) and (count % sample_frequency == 0):
+        if count % sample_frequency == 0:
             p = os.path.sep.join([args["output"], "{}.png".format(str(total_frame).zfill(5))])
             to_store = imutils.resize(frame, goal_size, goal_size)
-            if np.array(to_store).shape == (goal_size,goal_size):
-                print(to_store.shape)
+            # the resize result probably is not conform to the goal_size, some deviation can be occured ocassinally
+            # so we need to judge whether the resize result is standard or not
+            if np.array(to_store).shape == (goal_size, goal_size):
                 cv2.imwrite(p, to_store)
                 total_frame += 1
-
-    # show the output frame
-    cv2.imshow("Frame", frame)
-
-    # cv2.imshow("origin",orig)
-    key = cv2.waitKey(1) & 0xFF
-    #
-    # if key == ord("k"):
-    #     p = os.path.sep.join([args["output"], "{}.png".format(str(total_frame).zfill(5))])
-    #     cv2.imwrite(p, orig)
-    #     total_frame += 1
-    # # if the 'q' key was pressed, break from the loop
-    # elif key == ord("q"):
-    #     break
